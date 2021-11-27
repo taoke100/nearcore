7b71c2190bb5261bfcf99ba8e08961a88387e663
Piotr Mikulski <piotr@near.org>
2021-11-26 21:50:40 -0800

    Apply feedback

--- scripts/nayduck.py
+++ scripts/nayduck.py
@@ -50,10 +50,10 @@ def _parse_args():
                        '-i',
                        action='store_true',
                        help='Read test set from standard input.')
-    group.add_argument('--dry-run',
-                       '-d',
-                       action='store_true',
-                       help='Prints list of tests to execute, without doing anything')
+    parser.add_argument('--dry-run',
+                        '-d',
+                        action='store_true',
+                        help='Prints list of tests to execute, without doing anything')
     args = parser.parse_args()
 
     return args
